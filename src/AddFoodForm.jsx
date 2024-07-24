2116e2a5eba818137c8eb705564e3ede3cefae50
desmondt305 <desmondt918@gmail.com>
2024-07-24 11:04:26 -0700

    Last Commit

--- src/AddFoodForm.jsx
+++ src/AddFoodForm.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
-
+//Function that takes addfood as a prop.//
 const AddFoodForm = ({ addFood }) => {
   const initialFormState = { foodName: "", quantity: "" };
   const [food, setFood] = useState(initialFormState);
-
+  //Handles imnput changes and updates the state.//
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFood({ ...food, [name]: value });
   };
-
+  //Handles form submission//
   const handleSubmit = (event) => {
     event.preventDefault();
+    //Prevents from submitting if a field is empty.//
     if (!food.foodName || !food.quantity) return;
     addFood(food);
     setFood(initialFormState);
   };
-
+  //onSumbit is a event handler.//
   return (
     <form onSubmit={handleSubmit}>
       <label>Food Name:</label>
--- src/FoodItem.jsx
+++ src/FoodItem.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 const FoodItem = ({ food, deleteFood, editFood }) => {
-  // Generate the image URL based on the food name
+  // Creates the image URL based on the food name
   const imageUrl = `https://loremflickr.com/320/240/${food.foodName}`;
 
   return (
     <div className="food-item">
+      //Shows the food image//
       <img src={imageUrl} alt={food.foodName} />
       <div>
         <h3>{food.foodName}</h3>
         <p>Quantity: {food.quantity}</p>
+        //Button used to edit and delete the food when clicked.//
         <button onClick={() => editFood(food)}>Edit</button>
         <button onClick={() => deleteFood(food.id)}>Delete</button>
       </div>
